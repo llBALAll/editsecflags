801ac7a7e8ee9a7fea3bb4fddb16a4ed2cc1ce2e
llBALAll <43710708+llBALAll@users.noreply.github.com>
2020-04-14 04:08:59 -0300

    Update editsecflags.cpp

--- editsecflags.cpp
+++ editsecflags.cpp
@@ -7,7 +7,7 @@ BOOL EditSecFlags(LPCSTR path, bool ASLR, bool DEP) {
 	LOADED_IMAGE PE;
 
 	if (MapAndLoad(path, 0, &PE, 0, 0))	{
-		if (ASLR) PE.FileHeader->OptionalHeader.DllCharacteristics |= IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE;
+	if (ASLR) PE.FileHeader->OptionalHeader.DllCharacteristics |= IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE;
         else PE.FileHeader->OptionalHeader.DllCharacteristics &= ~IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE;
 
         if (DEP) PE.FileHeader->OptionalHeader.DllCharacteristics |= IMAGE_DLLCHARACTERISTICS_NX_COMPAT;
